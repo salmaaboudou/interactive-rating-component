29f1b62e5b4e5f3d21ae24dbc6ac5b9a02076a8d
Salma Aboudou <salmaaboudou@MacBook-Pro-de-Salma.local>
2022-08-14 23:25:08 +0200

    finalisation

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 // All the selectors
 let containerNumbers = document.querySelector('#container_numbers');
 let ratingButtons = document.querySelectorAll('.number');
-let submitButton = document.querySelector('#submit_button');
+let submitButton = document.querySelector('.submit_button');
 let ratingState = document.querySelector('#card1');
 let thankYouState = document.querySelector('#card2');
 let ratingValue = document.querySelector('.rValue');
 
-// Other variables
-let orangeUsed = 'rgb(251, 116, 19)';
+
+function ratingButtonSelected(){
+  let selectedButton = document.querySelector('.number.selected');
+  if (selectedButton !== null){
+    return true;
+  }
+  else{
+    return false;
+  }   
+};
 
 
 // Rating button 
 ratingButtons.forEach(button => {
   
   button.addEventListener('mouseover', () =>{
-    if (button.style.backgroundColor !== orangeUsed) {
-      button.style.backgroundColor = 'hsl(217, 12%, 63%)';
-      button.style.color = 'white';
-      button.style.cursor = 'pointer';
-    }
+    
+    button.classList.add('mouseover');
+    
   });
   
   button.addEventListener('mouseout', () =>{
-    if (button.style.backgroundColor !== orangeUsed) {
-        button.style.backgroundColor = '';
-        button.style.color = '';
-    }
+     
+    button.classList.remove('mouseover');
+    
   });
 
   button.addEventListener('click', () => {
     ratingButtons.forEach(button => {
-      button.style.backgroundColor = '';
-      button.style.color = '';
+      button.classList.remove('selected');
     });
 
-    button.style.backgroundColor = orangeUsed;
-    button.style.color = 'white';
+    button.classList.add('selected');
+
   });
 
 
@@ -44,20 +48,24 @@ ratingButtons.forEach(button => {
 // Submit button
 
 submitButton.addEventListener('mouseover', () => {
-  submitButton.style.backgroundColor = 'white';
-  submitButton.style.color = 'orange';
-  submitButton.style.cursor = 'pointer';
+  submitButton.classList.add('mouseover');
 });
 
 submitButton.addEventListener('mouseout', () => {
-  submitButton.style.backgroundColor = '';
-  submitButton.style.color = '';
+  submitButton.classList.remove('mouseover');
 });
+
 
 submitButton.addEventListener('click', () =>{
 
-  ratingState.style.display = "none";
-  thankYouState.style.display = "block";
+  if (ratingButtonSelected()) {
+    ratingState.style.display = "none";
+    thankYouState.style.display = "block";
+  }
+
+  else{
+    alert('Please give a rating before submitting');
+  }
 
 });
 
@@ -67,13 +75,7 @@ containerNumbers.addEventListener('click', (e) => {
   // event.target : retourne l'élément qui a déclenché l'évenement -> le texte à l'int du button appuyé
   let value = e.target.innerText;
   // On ajoute la valeur de "value" récupéré dans le texte html de class="rValue"
-  ratingValue.textContent = value;
-
-  if (value == '') {
-    
-    ratingValue.textContent.display = "none";
-    ratingValue.textContent = 'Error';
-  } 
+  ratingValue.textContent = value; 
     
 });
     
